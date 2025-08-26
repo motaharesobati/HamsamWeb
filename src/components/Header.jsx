33c8f5839d1c774347d7ec770e725406ee36aa8d
motaharesobati <motahareh.sobati@gmail.com>
2025-08-26 12:07:40 +0330

    change these file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,10 +15,13 @@ function Header(){
             <div className="row align-items-center py-4" >
                     <div className="col-md-3 persionName">سوپر اپلیکیشن هم سام</div>
                     <div className="col-md-5">
-                        <ul className="listOfHeader  d-flex gap-0 list-unstyled m-0 justify-content-center">
+                        <ul className="listOfHeader  d-flex gap-4 list-unstyled m-0 justify-content-center" 
+                        style={{flexWrap: "nowrap", whiteSpace: "nowrap"}}>
                             {list.map((item , index) => {
                                 return(
-                                    <li key={index} className="col">{item}</li>
+                                    <li key={index} className="nav-item">
+                                         <a href="#" className="nav-link">{item}</a>
+                                    </li>
                                 )
                             })}
                         </ul>
