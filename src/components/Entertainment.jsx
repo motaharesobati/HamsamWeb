307e516f625fc5f81502fc09aba96f823226cfc9
motaharesobati <motahareh.sobati@gmail.com>
2025-08-26 10:32:11 +0330

    change these file

---
+++ src/components/Entertainment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../CssComponent/Entertainment.css';
+import icon from '../images/icon-park-outline_game-ps.png';
+
+function Entertainment(){
+    return(
+        <>
+        <div className="icon"><img src={icon}/></div>
+
+        </>
+    )
+}
+export default Entertainment;
